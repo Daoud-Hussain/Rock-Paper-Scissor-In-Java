1fad760de9269e604b4b83fb5ed15a6ca859cab3
Daoud-Hussain <87219816+Daoud-Hussain@users.noreply.github.com>
2021-12-03 20:36:45 +0500

    Update JavaRockPaperScissorGame.java

--- JavaRockPaperScissorGame.java
+++ JavaRockPaperScissorGame.java
@@ -10,7 +10,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Practice{
+public class JavaRockPaperScissorGame{
     public static void main(String[] args) {
         
         //Initalizing all required variables
